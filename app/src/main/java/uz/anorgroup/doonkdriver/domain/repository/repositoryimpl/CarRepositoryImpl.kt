0cbd45e1840fa2aea37aac7657b4e9ef46cae61a
anvar6996 <juraqulovanvar69@gmail.com>
2021-12-19 01:20:20 +0500

    Anvar

--- app/src/main/java/uz/anorgroup/doonkdriver/domain/repository/repositoryimpl/CarRepositoryImpl.kt
+++ app/src/main/java/uz/anorgroup/doonkdriver/domain/repository/repositoryimpl/CarRepositoryImpl.kt
@@ -1,6 +1,10 @@
 package uz.anorgroup.doonkdriver.domain.repository.repositoryimpl
 
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 import retrofit2.Response
 import uz.anorgroup.doonkdriver.data.api.CarApi
 import uz.anorgroup.doonkdriver.data.pref.MyPref
@@ -10,23 +14,67 @@ import uz.anorgroup.doonkdriver.domain.repository.CarRepository
 import javax.inject.Inject
 
 class CarRepositoryImpl @Inject constructor(private val api: CarApi, private val pref: MyPref) : CarRepository {
-    override fun carCreate(data: CreateCarRequest): Flow<Response<CreateCarResponce>> {
-        TODO("Not yet implemented")
-    }
 
-    override fun brands(): Flow<Response<BrandsResponce>> {
-        TODO("Not yet implemented")
-    }
 
-    override fun carModels(): Flow<Response<ModelResponce>> {
-        TODO("Not yet implemented")
-    }
+    override fun carCreate(data: CreateCarRequest): Flow<Result<CreateCarResponce>> = flow {
+        val responce = api.carCreate(data)
+        if (responce.isSuccessful) {
+            emit(Result.success<CreateCarResponce>(responce.body()!!))
+        } else {
+            emit(Result.failure(Throwable(responce.errorBody().toString())))
+        }
+    }.catch {
+        val errorMessage = Throwable("Sever bilan muammo bo'ldi")
+        emit(Result.failure(errorMessage))
+    }.flowOn(Dispatchers.IO)
 
-    override fun typesOfTransport(): Flow<Response<TypeTransportResponce>> {
-        TODO("Not yet implemented")
-    }
 
-    override fun typesOfBody(): Flow<Response<TypeOfBodyResponce>> {
-        TODO("Not yet implemented")
-    }
+    override fun brands(): Flow<Result<BrandsResponce>> = flow {
+        val responce = api.brands()
+        if (responce.isSuccessful) {
+            emit(Result.success<BrandsResponce>(responce.body()!!))
+        } else {
+            emit(Result.failure(Throwable(responce.errorBody().toString())))
+        }
+    }.catch {
+        val errorMessage = Throwable("Sever bilan muammo bo'ldi")
+        emit(Result.failure(errorMessage))
+    }.flowOn(Dispatchers.IO)
+
+    override fun carModels(): Flow<Result<ModelResponce>> = flow {
+        val responce = api.brands()
+        if (responce.isSuccessful) {
+            emit(Result.success<ModelResponce>(responce.body()!!))
+        } else {
+            emit(Result.failure(Throwable(responce.errorBody().toString())))
+        }
+    }.catch {
+        val errorMessage = Throwable("Sever bilan muammo bo'ldi")
+        emit(Result.failure(errorMessage))
+    }.flowOn(Dispatchers.IO)
+
+    override fun typesOfTransport(): Flow<Result<TypeTransportResponce>> = flow {
+        val responce = api.brands()
+        if (responce.isSuccessful) {
+            emit(Result.success<TypeTransportResponce>(responce.body()!!))
+        } else {
+            emit(Result.failure(Throwable(responce.errorBody().toString())))
+        }
+    }.catch {
+        val errorMessage = Throwable("Sever bilan muammo bo'ldi")
+        emit(Result.failure(errorMessage))
+    }.flowOn(Dispatchers.IO)
+
+    override fun typesOfBody(): Flow<Result<TypeOfBodyResponce>> = flow {
+        val responce = api.brands()
+        if (responce.isSuccessful) {
+            emit(Result.success<TypeOfBodyResponce>(responce.body()!!))
+        } else {
+            emit(Result.failure(Throwable(responce.errorBody().toString())))
+        }
+    }.catch {
+        val errorMessage = Throwable("Sever bilan muammo bo'ldi")
+        emit(Result.failure(errorMessage))
+    }.flowOn(Dispatchers.IO)
 }
+
