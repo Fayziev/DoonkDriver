44f47ce9098bed2e8c5c32ceeeedd89967a29cda
anvar6996 <juraqulovanvar69@gmail.com>
2021-12-20 18:07:16 +0500

    Anvar

--- app/src/main/java/uz/anorgroup/doonkdriver/presentation/adapters/AvtoTypeAdapter.kt
+++ app/src/main/java/uz/anorgroup/doonkdriver/presentation/adapters/AvtoTypeAdapter.kt
@@ -8,18 +8,18 @@ import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import by.kirich1409.viewbindingdelegate.viewBinding
 import uz.anorgroup.doonkdriver.R
-import uz.anorgroup.doonkdriver.data.responce.car.TransportData
+import uz.anorgroup.doonkdriver.data.responce.car.AvtoTypeData
 import uz.anorgroup.doonkdriver.databinding.ItemTransportTypeBinding
 
-class TransportTypeAdapter : ListAdapter<List<TransportData>, TransportTypeAdapter.HistoryVH>(MyDifUtils) {
-    private var itemListener: ((List<TransportData>) -> Unit)? = null
+class AvtoTypeAdapter : ListAdapter<List<AvtoTypeData>, AvtoTypeAdapter.HistoryVH>(MyDifUtils) {
+    private var itemListener: ((List<AvtoTypeData>) -> Unit)? = null
 
-    object MyDifUtils : DiffUtil.ItemCallback<List<TransportData>>() {
-        override fun areItemsTheSame(oldItem: List<TransportData>, newItem: List<TransportData>): Boolean {
+    object MyDifUtils : DiffUtil.ItemCallback<List<AvtoTypeData>>() {
+        override fun areItemsTheSame(oldItem: List<AvtoTypeData>, newItem: List<AvtoTypeData>): Boolean {
             return oldItem == newItem
         }
 
-        override fun areContentsTheSame(oldItem: List<TransportData>, newItem: List<TransportData>): Boolean {
+        override fun areContentsTheSame(oldItem: List<AvtoTypeData>, newItem: List<AvtoTypeData>): Boolean {
             return oldItem == newItem
         }
     }
@@ -34,7 +34,7 @@ class TransportTypeAdapter : ListAdapter<List<TransportData>, TransportTypeAdapt
         }
 
         fun load() {
-            val value = getItem(absoluteAdapterPosition) as TransportData
+            val value = getItem(absoluteAdapterPosition) as AvtoTypeData
             bind.transportType.text = value.name
         }
     }
--- app/src/main/java/uz/anorgroup/doonkdriver/presentation/adapters/BrandsTypeAdapter.kt
+++ app/src/main/java/uz/anorgroup/doonkdriver/presentation/adapters/BrandsTypeAdapter.kt
@@ -8,18 +8,18 @@ import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import by.kirich1409.viewbindingdelegate.viewBinding
 import uz.anorgroup.doonkdriver.R
-import uz.anorgroup.doonkdriver.data.responce.car.TransportData
+import uz.anorgroup.doonkdriver.data.responce.car.CarData
 import uz.anorgroup.doonkdriver.databinding.ItemTransportTypeBinding
 
-class TransportTypeAdapter : ListAdapter<List<TransportData>, TransportTypeAdapter.HistoryVH>(MyDifUtils) {
-    private var itemListener: ((List<TransportData>) -> Unit)? = null
+class BrandsTypeAdapter : ListAdapter<List<CarData>, BrandsTypeAdapter.HistoryVH>(MyDifUtils) {
+    private var itemListener: ((List<CarData>) -> Unit)? = null
 
-    object MyDifUtils : DiffUtil.ItemCallback<List<TransportData>>() {
-        override fun areItemsTheSame(oldItem: List<TransportData>, newItem: List<TransportData>): Boolean {
+    object MyDifUtils : DiffUtil.ItemCallback<List<CarData>>() {
+        override fun areItemsTheSame(oldItem: List<CarData>, newItem: List<CarData>): Boolean {
             return oldItem == newItem
         }
 
-        override fun areContentsTheSame(oldItem: List<TransportData>, newItem: List<TransportData>): Boolean {
+        override fun areContentsTheSame(oldItem: List<CarData>, newItem: List<CarData>): Boolean {
             return oldItem == newItem
         }
     }
@@ -34,7 +34,7 @@ class TransportTypeAdapter : ListAdapter<List<TransportData>, TransportTypeAdapt
         }
 
         fun load() {
-            val value = getItem(absoluteAdapterPosition) as TransportData
+            val value = getItem(absoluteAdapterPosition) as CarData
             bind.transportType.text = value.name
         }
     }
