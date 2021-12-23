b6690f41c94afd6fb45792e6aa5a03e8a47da76a
Fayziev <fayziyevasadbek19@gmail.com>
2021-12-23 11:43:42 +0500

    Asadbek

--- app/src/main/java/uz/anorgroup/doonkdriver/presentation/screens/IntermediateScreen.kt
+++ app/src/main/java/uz/anorgroup/doonkdriver/presentation/screens/IntermediateScreen.kt
@@ -7,8 +7,9 @@ import androidx.navigation.fragment.findNavController
 import by.kirich1409.viewbindingdelegate.viewBinding
 import dagger.hilt.android.AndroidEntryPoint
 import uz.anorgroup.doonkdriver.R
-import uz.anorgroup.doonkdriver.data.request.car.CarSeet
+import uz.anorgroup.doonkdriver.data.request.car.CreateCarRequest
 import uz.anorgroup.doonkdriver.databinding.ScreenIntermediateBinding
+import uz.anorgroup.doonkdriver.utils.timber
 
 @AndroidEntryPoint
 class IntermediateScreen : Fragment(R.layout.screen_intermediate) {
@@ -16,6 +17,9 @@ class IntermediateScreen : Fragment(R.layout.screen_intermediate) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val bundle = requireArguments()
+        val data: CreateCarRequest = bundle.getParcelable("data")!!
+        timber(data.carSeet?.size.toString())
         bind.nextBt.setOnClickListener {
             findNavController().navigate(R.id.action_screenIntermediate_to_whenScreen)
         }
